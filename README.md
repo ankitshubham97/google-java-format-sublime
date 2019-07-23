37093c08e41615211d6f47dcf32eb29403e01c81
Ankit Shubham <ankitshubham97@gmail.com>
2019-07-24 01:34:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# google-java-format-sublime
+A sublime plugin for formatting java code files with google-java-format
---
+++ google-java-format-sublime.py
@@ -0,0 +1,10 @@
+import sublime, sublime_plugin
+import subprocess
+import sys
+
+class GoogleJavaFormatCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		p = subprocess.Popen(['java', '-jar', './artifacts/google-java-format-1.8-SNAPSHOT-all-deps.jar', '-i', str(self.view.file_name())], stdout=subprocess.PIPE, stderr=None, stdin=None)
+		stdout, stderr = p.communicate()
+		if p.returncode != 0:
+			sys.exit(p.returncode);
