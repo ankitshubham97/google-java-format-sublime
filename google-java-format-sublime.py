b143fe6a8d21896dedc6759873d1e88af453146f
Ankit Shubham <ankitshubham97@gmail.com>
2019-07-28 15:42:52 +0530

    Remove semicolon

--- google-java-format-sublime.py
+++ google-java-format-sublime.py
@@ -7,4 +7,4 @@ class GoogleJavaFormatCommand(sublime_plugin.TextCommand):
 		p = subprocess.Popen(['java', '-jar', './artifacts/google-java-format-1.8-SNAPSHOT-all-deps.jar', '-i', str(self.view.file_name())], stdout=subprocess.PIPE, stderr=None, stdin=None)
 		stdout, stderr = p.communicate()
 		if p.returncode != 0:
-			sys.exit(p.returncode);
+			sys.exit(p.returncode)
